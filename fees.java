2f6438c572d54f0a590022af536f23330d044f2d
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2024-07-21 19:59:11 +0530

    Create fees.java

---
+++ fees.java
@@ -0,0 +1,13 @@
+//April 2024 - March 2025
+
+import java.util.Scanner;
+
+public class Fees
+  {
+    public static void main (String[] args)
+    {
+      String English, Bengali, Maths, Geography, Computer, PhysicalEducation;
+      Scanner scanner = new Scanner (System.in);
+      System.out.println("Enter Month:");
+    }
+  }
