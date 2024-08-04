3782da6f35cc06cd7abb237b1e066611e6826d63
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2024-08-04 19:03:34 +0530

    Update fees.java

--- fees.java
+++ fees.java
@@ -6,8 +6,49 @@ public class Fees
   {
     public static void main (String[] args)
     {
-      String English, Bengali, Maths, Geography, Computer, PhysicalEducation;
       Scanner scanner = new Scanner (System.in);
       System.out.println("Enter Month:");
+      String month = scanner.nextLine();
+
+      switch (month) {
+            case "January":
+                January();
+                break;
+            case "February":
+                February();
+                break;
+            case "March":
+                March();
+                break;
+            case "April":
+                April();
+                break;
+            case "May":
+                May();
+                break;
+            case "June":
+                June();
+                break;
+           case "July":
+                July();
+                break;
+            case "August":
+                August();
+                break;
+            case "September":
+                September();
+                break;
+            case "October":
+                October();
+                break;
+            case "November":
+                November();
+                break;
+            case "December":
+                December();
+                break;
+            default:
+                System.out.println("Invalid month name. Please enter a valid month name.");
+                break;
+      }
     }
-  }
